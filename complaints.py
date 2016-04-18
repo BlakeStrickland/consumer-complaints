506230b4748755fd04b4d05e5b154cc7cc66a1be
Blake Strickland <Stricklandb02@gmail.com>
2016-04-18 15:35:51 -0400

    Number of complaints by company response

--- complaints.py
+++ complaints.py
@@ -15,3 +15,10 @@ complaints_by_product = complaints[['Company','Complaint ID']].copy()
 cbp = complaints_by_product.rename(columns={'Complaint ID': 'ID'}).copy()
 c = cbp.groupby("Company").size()
 c.sort_values(ascending=False).head(10)
+
+'''Number of complaints by company response'''
+complaints = pd.read_csv('complaints_dec_2014.csv')
+complaints_by_product = complaints[['Company response','Complaint ID']].copy()
+cbp = complaints_by_product.rename(columns={'Complaint ID': 'ID'}).copy()
+c = cbp.groupby("Company response").size()
+c.sort_values(ascending=False)

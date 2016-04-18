e3074ea3ad77f2cb6c942fa1fdbcd0dece91c5d3
Blake Strickland <Stricklandb02@gmail.com>
2016-04-18 15:24:13 -0400

    Group products by complaints

--- complaints.py
+++ complaints.py
@@ -2,6 +2,10 @@ import numpy as np
 import pandas as pd
 from pandas import DataFrame, Series
 
+'''Complaints per product'''
 complaints = pd.read_csv('complaints_dec_2014.csv')
-
-print(complaints.head())
+complaints_by_product = complaints[['Product','Complaint ID']].copy()
+print(complaints_by_product)
+cbp = complaints_by_product.rename(columns={'Complaint ID': 'ID'}).copy()
+c = cbp.groupby("Product").size()
+c.sort_values(ascending=False)
